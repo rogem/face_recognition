17460ef767727ab10e3eb27e4aa23ac06d1f803e
rogem <57857093+rogem@users.noreply.github.com>
2022-09-14 16:25:30 +0800

    Add files via upload

---
+++ face_recog.py
@@ -0,0 +1,288 @@
+from tkinter import *
+from tkinter import font, ttk, messagebox
+from PIL import ImageTk, Image 
+import time
+import customtkinter
+
+w=Tk()
+
+#Using piece of code from old splash screen
+width_of_window = 1000
+height_of_window = 500
+screen_width = w.winfo_screenwidth()
+screen_height = w.winfo_screenheight()
+x_coordinate = (screen_width/2)-(width_of_window/2)
+y_coordinate = (screen_height/2)-(height_of_window/2)
+w.geometry("%dx%d+%d+%d" %(width_of_window,height_of_window,x_coordinate,y_coordinate))
+w.configure(bg='#ED1B76')
+w.overrideredirect(1) #for hiding titlebar
+
+
+
+Frame(w, width=1000, height=500).place(x=0,y=0)
+img= (Image.open("pic/1.png"))
+resized_image_splash= img.resize((1000,500))
+photo= ImageTk.PhotoImage(resized_image_splash)
+# label = Label(w, image = photo)
+# label.pack()
+
+# label2=Label(w, text='Loading', fg='white', bg='#dab015') #decorate it 
+# label2.configure(font=("Calibri", 17))
+# label2.place(x=10,y=465)
+
+#making animation
+
+image_a=ImageTk.PhotoImage(Image.open('pic/dot2.png'))
+image_b=ImageTk.PhotoImage(Image.open('pic/dot1.png'))
+
+
+# for i in range(5):
+#     # loading text
+#     label2=Label(w, text='Loading', fg='white', bg='#dab015') #decorate it 
+#     label2.configure(font=("Calibri", 17))
+#     label2.place(x=10,y=465)
+#     # background image
+#     label = Label(w, image = photo)
+#     label.pack()
+#     # loading dot animation
+#     l1=Label(w, image=image_a, border=0, relief=SUNKEN).place(x=90, y=480)
+#     l2=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=110, y=480)
+#     l3=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=130, y=480)
+#     l4=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=150, y=480)
+#     w.update_idletasks()
+#     time.sleep(0.5)
+
+#     # loading text
+#     label2=Label(w, text='Loading', fg='white', bg='#dab015') #decorate it 
+#     label2.configure(font=("Calibri", 17))
+#     label2.place(x=10,y=465)
+#     # background image
+#     label = Label(w, image = photo)
+#     label.pack()
+#     # loading dot animation
+#     l1=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=90, y=480)
+#     l2=Label(w, image=image_a, border=0, relief=SUNKEN).place(x=110, y=480)
+#     l3=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=130, y=480)
+#     l4=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=150, y=480)
+#     w.update_idletasks()
+#     time.sleep(0.5)
+
+#     # loading text
+#     label2=Label(w, text='Loading', fg='white', bg='#dab015') #decorate it 
+#     label2.configure(font=("Calibri", 17))
+#     label2.place(x=10,y=465)
+#     # background image
+#     label = Label(w, image = photo)
+#     label.pack()
+#     # loading dot animation
+#     l1=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=90, y=480)
+#     l2=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=110, y=480)
+#     l3=Label(w, image=image_a, border=0, relief=SUNKEN).place(x=130, y=480)
+#     l4=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=150, y=480)
+#     w.update_idletasks()
+#     time.sleep(0.5)
+
+#     # loading text
+#     label2=Label(w, text='Loading', fg='white', bg='#dab015') #decorate it 
+#     label2.configure(font=("Calibri", 17))
+#     label2.place(x=10,y=465)
+#     # background image
+#     label = Label(w, image = photo)
+#     label.pack()
+#     # loading dot animation
+#     l1=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=90, y=480)
+#     l2=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=110, y=480)
+#     l3=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=130, y=480)
+#     l4=Label(w, image=image_a, border=0, relief=SUNKEN).place(x=150, y=480)
+#     w.update_idletasks()
+#     time.sleep(0.5)
+
+#new window to open
+def new_win():
+
+    customtkinter.set_appearance_mode("Light")  # Modes: "System" (standard), "Dark", "Light"
+    customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+    main_window=customtkinter.CTk()
+    main_window.title('main window')
+    # main_window.geometry('1000x500')
+    main_window.state('zoomed')
+    # main_window.rowconfigure(0, weight=2)
+    # main_window.columnconfigure(1, weight=1)
+    
+    page1 = Frame(main_window)
+    page2 = Frame(main_window)
+    page3 = Frame(main_window)
+    page4 = Frame(main_window)
+    attendace_record = Frame(main_window)
+
+    for frame in (page1, page2, page3, page4, attendace_record):
+        frame.grid(row=0, column=0, sticky='nsew')
+
+    def show_frame(frame):
+        frame.tkraise()
+
+    show_frame(attendace_record)
+
+    # ============= Page 1 =========
+
+        # open background image
+    page1.pg1_image = Image.open('pic/2.png')
+    page1.pg1_resize_image = page1.pg1_image.resize((1362, 692))
+    page1.photo = ImageTk.PhotoImage(page1.pg1_resize_image)
+    page1.pg1_bg_img_lb = Label(page1, image = page1.photo)
+    page1.pg1_bg_img_lb.pack()
+    
+        # Login Button
+    pg1_login_img_btn = PhotoImage(file = "pic/btn_login.png")
+    pg1_button_admin = Button(page1,image=pg1_login_img_btn, borderwidth=0, bg='#1f2a76',command=lambda: show_frame(page2))
+    pg1_button_admin.place(x=99, y=422)
+
+        # Biomettric Button
+    biometric_img_btn = PhotoImage(file = "pic/bttn_biometric.png")
+    pg1_button_bio = Button(page1,image=biometric_img_btn, borderwidth=0,bg='#1f2a76', command=lambda: show_frame(page2))
+    pg1_button_bio.place(x=99, y=256)
+
+    # ============= Page 2 =========
+
+        # open background image
+    page2.pg2_image = Image.open('pic/3.png')
+    page2.pg2_resize_image = page2.pg2_image.resize((1362, 692))
+    page2.photo = ImageTk.PhotoImage(page2.pg2_resize_image)
+    page2.pg2_bg_img_lb = Label(page2, image = page2.photo)
+    page2.pg2_bg_img_lb.pack()
+
+        # Employee Button
+    employee_img_btn = PhotoImage(file = "pic/btn_employee.png")
+    pg2_button_bio = Button(page2,image=employee_img_btn, borderwidth=0,bg='#1f2a76', command=lambda: show_frame(page3))
+    pg2_button_bio.place(x=99, y=256)
+
+        # Admin Button
+    admin_img_btn = PhotoImage(file = "pic/bttn_admin.png")
+    pg2_button_admin = Button(page2,image=admin_img_btn, borderwidth=0, bg='#1f2a76',command=lambda: show_frame(page3))
+    pg2_button_admin.place(x=99, y=422)
+
+    # ======== Page 3 ===========
+
+        # open background image
+    page3.pg3_image = Image.open('pic/4.png')
+    page3.pg3_resize_image = page3.pg3_image.resize((1362, 692))
+    page3.photo = ImageTk.PhotoImage(page3.pg3_resize_image)
+    page3.pg3_bg_img_lb = Label(page3, image = page3.photo)
+    page3.pg3_bg_img_lb.pack()
+
+        # Text Box Username and Password
+    pg3_txtbox_username = Entry(page3, borderwidth=0, width=16, font=('Arial',30))
+    pg3_txtbox_username.insert(0,"Username")
+    pg3_txtbox_username.place(x=116, y=256, height=92)
+
+    pg3_txtbox_pass = Entry(page3, borderwidth=0, width=16, font=('Arial', 30), show='*')
+    pg3_txtbox_pass.insert(0,"Password")
+    pg3_txtbox_pass.place(x=116, y=422, height=90)
+
+        # Account verification
+    def verify():
+        try:
+            with open("Account.txt", "r") as f:
+                info = f.readlines()
+                i  = 0
+                for e in info:
+                    u, p =e.split(",")
+                    if u.strip() == pg3_txtbox_username.get() and p.strip() == pg3_txtbox_pass.get():
+                        show_frame(page4)
+                        i = 1
+                        break
+                if i==0:
+                    messagebox.showinfo("Error", "Please provide correct username and password!!")
+        except:
+            messagebox.showinfo("Error", "Please provide correct username and password!!")
+
+        # Login Button
+    login_img_btn = PhotoImage(file = "pic/login.png")
+    pg3_button = Button(page3, image=login_img_btn, borderwidth=0, bg='#1f2a76', command=verify)
+    pg3_button.place(x=180, y=557)
+
+        # show and hide Password
+    def show_password():
+        if  pg3_txtbox_pass.cget('show') =='*':
+            pg3_txtbox_pass.configure(show='')
+        else:
+            pg3_txtbox_pass.configure(show='*')
+    check_button = Checkbutton(page3, text="show password",bg="#1f2a76", command=show_password, font="Arial", activebackground="#1f2a76",)
+    check_button.place(x=116,y=520)
+
+    # ============= Page 4 =========
+
+        # open background image
+    page4.pg4_image = Image.open('pic/7.png')
+    page4.pg4_resize_image = page4.pg4_image.resize((1362, 692))
+    page4.photo = ImageTk.PhotoImage(page4.pg4_resize_image)
+    page4.pg4_bg_img_lb = Label(page4, image = page4.photo)
+    page4.pg4_bg_img_lb.pack()
+    
+        # Faculty Information Button
+    faculty_info_btn = PhotoImage(file = "pic/faculty_info.png")
+    pg4_button_faculty = customtkinter.CTkButton(master=page4,image=faculty_info_btn, text="" ,
+                                                corner_radius=20, fg_color="#00436e",hover_color="#006699", command=lambda: show_frame(page2))
+    pg4_button_faculty.place(x=100, y=314, height=134,width=562)
+
+        # Faculty Information Button
+    faculty_info_btn = PhotoImage(file = "pic/faculty_info.png")
+    pg4_button_faculty = customtkinter.CTkButton(master=page4,image=faculty_info_btn, text="" ,
+                                                corner_radius=30, fg_color="#00436e",hover_color="#006699", command=lambda: show_frame(page2))
+    pg4_button_faculty.place(x=100, y=314, height=134,width=562)
+
+        # Attendace Record Button
+    att_rec_btn = PhotoImage(file = "pic/attendance_rec.png")
+    pg4_button_att_rec = customtkinter.CTkButton(master=page4,image=att_rec_btn, text="" ,
+                                                corner_radius=30, fg_color="#cc9900",hover_color="#fdca34", command=lambda: show_frame(attendace_record))
+    pg4_button_att_rec.place(x=100, y=469, height=178,width=330)
+
+        # About Button
+    about_btn = PhotoImage(file = "pic/about.png")
+    pg4_button_photo = customtkinter.CTkButton(master=page4,image=about_btn, text="" ,
+                                                corner_radius=30, fg_color="#cc9900",hover_color="#fdca34", command=lambda: show_frame(page2))
+    pg4_button_photo.place(x=456, y=469, height=178,width=197)
+
+        # Photo Button
+    photo_btn = PhotoImage(file = "pic/photo.png")
+    pg4_button_train_img = customtkinter.CTkButton(master=page4,image=photo_btn, text="" ,
+                                                corner_radius=30, fg_color="#cc9900",hover_color="#fdca34", command=lambda: show_frame(page2))
+    pg4_button_train_img.place(x=683, y=315, height=333,width=348)
+
+        # Developers Button
+    developers_btn = PhotoImage(file = "pic/developers.png")
+    pg4_button_developers = customtkinter.CTkButton(master=page4,image=developers_btn, text="" ,
+                                                corner_radius=20, fg_color="#00436e",hover_color="#006699", command=lambda: show_frame(page2))
+    pg4_button_developers.place(x=1048, y=314, height=134,width=246)
+
+        # Logout Button
+    logout_btn = PhotoImage(file = "pic/logout.png")
+    pg4_button_logout = customtkinter.CTkButton(master=page4,image=logout_btn, text="" ,
+                                                corner_radius=30, fg_color="#f0f0f0",hover_color="#6699cc", command=lambda: show_frame(page2))
+    pg4_button_logout.place(x=1075, y=469, height=178,width=197)
+
+    # ============= Attendace Record =========
+
+        # open background image
+    attendace_record.att_rec_image = Image.open('pic/8.png')
+    attendace_record.att_rec_resize_image = attendace_record.att_rec_image.resize((1362, 692))
+    attendace_record.photo = ImageTk.PhotoImage(attendace_record.att_rec_resize_image)
+    attendace_record.att_rec_bg_img_lb = Label(attendace_record, image = attendace_record.photo)
+    attendace_record.att_rec_bg_img_lb.pack()
+
+        # Mathematics Faculty Button
+    # math_fac_btn = PhotoImage(file = "pic/math_faculty.png")
+    # att_rec_button_math_fac = customtkinter.CTkButton(master=attendace_record,image=math_fac_btn, text="" ,
+    #                                             corner_radius=20,bg='#ffffff', fg_color="#00436e",hover_color="#006699", command=lambda: show_frame(page2))
+    # att_rec_button_math_fac.place(x=254, y=254, height=99,width=413)
+
+    math_fac_btn = PhotoImage(file = "pic/btn_math_faculty.png")
+    att_rec_button_math_fac = Button(attendace_record,image=math_fac_btn, borderwidth=0, bg='#ffffff',command=lambda: show_frame(page2))
+    att_rec_button_math_fac.place(x=254, y=254)
+    
+    main_window.mainloop()
+
+w.destroy()
+new_win()
+w.mainloop()
